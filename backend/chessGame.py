4c94a93b23a99bfbf2506df372925939bf6d45d9
jared <jaredg.117@gmail.com>
2021-01-30 11:07:13 -0700

    API and Classes

---
+++ backend/chessGame.py
@@ -0,0 +1,23 @@
+class chessGame:
+
+    gameID = 0
+    chessBoard = 0
+    blackPlayer = ""
+    whitePlayer = ""
+    completed = False
+
+    def __init__(self):
+
+        return
+
+    def getUserInput(self, info):
+
+        # Read Socket
+        # Check move with board
+        # Return if move made or not
+
+        return
+
+    def readSocket(self):
+
+        return
---
+++ backend/chessPiece.py
@@ -0,0 +1,16 @@
+class chessPiece:
+
+    location = (0, 0)
+    id = ""
+    name = ""
+
+    def makeMove(self):
+
+        return
+
+
+class vanguard (chessPiece):
+
+    def makeMove(self, position):
+        # Check if position fits inside definition of piece
+        return False
